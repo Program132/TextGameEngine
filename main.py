5cd64e2950de7e201f84a6637ed255d1109bb5a5
Program <67320703+Program132@users.noreply.github.com>
2024-10-17 19:04:56 +0200

    Test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from src.Engine import Engine
 from src.Level import Level
-from src.Point import Point
 from src.Segment import Segment
 from src.Triangle import Triangle
 
@@ -8,8 +7,8 @@ engine = Engine(50, 5, ".")
 
 mainLevel = Level(engine)
 
-mainLevel.addObject(Point(0, 0, "*"))
-mainLevel.addObject(Triangle(Segment(1,1,3,1), 1,0))
+mainLevel.addObject(Triangle(Segment(10, 4, 12, 4), 11, 3))
+mainLevel.addObject(Triangle(Segment(20, 3, 22, 3), 21, 4))
 
 engine.setCurrentLevel(mainLevel)
 engine.display()
