2413b3148a9d1f427f9e3b0fd4e4cd4cbc047f13
Program <67320703+Program132@users.noreply.github.com>
2024-10-18 08:34:47 +0200

    Test

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+from src.Cercle import Cercle
 from src.Engine import Engine
 from src.Level import Level
-from src.Segment import Segment
-from src.Triangle import Triangle
 
 engine = Engine(50, 5, ".")
 
 mainLevel = Level(engine)
 
-mainLevel.addObject(Triangle(Segment(10, 4, 12, 4), 11, 3))
-mainLevel.addObject(Triangle(Segment(20, 3, 22, 3), 21, 4))
+mainLevel.addObject(Cercle(10, 3, 2))
 
 engine.setCurrentLevel(mainLevel)
 engine.display()
