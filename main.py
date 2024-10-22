fa2a3c6ac46e8c2ff0c7caa178504e0eee33f79a
Program <67320703+Program132@users.noreply.github.com>
2024-10-22 19:48:39 +0200

    Test

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 from src.Engine import Engine
 from src.Level import Level
+import keyboard
+import time
+from src.Point import Point
 
-engine = Engine(50, 5, " ")
-
+engine = Engine(50, 5, ".")
 mainLevel = Level(engine)
 
+PLAYER_POINT = Point(3, 1)
 
+mainLevel.addObject(PLAYER_POINT)
 engine.setCurrentLevel(mainLevel)
-
 engine.display()
+
+
+def manageKeysPressed(event):
+    if event.name == "z":
+        mainLevel.removeObject(PLAYER_POINT.getX(), PLAYER_POINT.getY())
+        PLAYER_POINT.setY(PLAYER_POINT.getY() - 1)
+        mainLevel.addObject(PLAYER_POINT)
+        engine.refresh()
+    elif event.name == "s":
+        mainLevel.removeObject(PLAYER_POINT.getX(), PLAYER_POINT.getY())
+        PLAYER_POINT.setY(PLAYER_POINT.getY() + 1)
+        mainLevel.addObject(PLAYER_POINT)
+        engine.refresh()
+
+
+keyboard.on_press(manageKeysPressed)
+
+running = True
+while running:
+    if keyboard.is_pressed("esc"):
+        running = False
+
+    time.sleep(0.001)
