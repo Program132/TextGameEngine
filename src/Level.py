e813c7040443e58055eeee5bcfe1599e9db7d19c
Program <67320703+Program132@users.noreply.github.com>
2024-10-22 19:48:24 +0200

    Fixed Level class

--- src/Level.py
+++ src/Level.py
@@ -5,52 +5,52 @@ from src.Triangle import Triangle
 from src.Rectangle import Rectangle
 from src.Circle import Circle
 
+
 class Level:
     def __init__(self, e: Engine = None):
         self.objects = []
         self.engine = e
+        self.points_map = {}
 
     def addObject(self, o):
         self.objects.append(o)
 
     def removeObject(self, x: int, y: int):
-        for i in range(0, len(self.objects)):
+        # Supprimer la position correspondante dans points_map
+        if (x, y) in self.points_map:
+            del self.points_map[(x, y)]
+
+        # Supprimer l'objet de la liste
+        for i in range(len(self.objects)):
             o = self.objects[i]
             if isinstance(o, Point) and o.getX() == x and o.getY() == y:
                 del self.objects[i]
-            elif isinstance(o, Segment) and o.getX1() == x and o.getY1() == y:
-                del self.objects[i]
-            elif isinstance(o, Rectangle) and o.getUpX() == x and o.getUpY() == y:
-                del self.objects[i]
-            elif isinstance(o, Triangle) and o.getUpPoint().getX() == x and o.getUpPoint().getY() == y:
-                del self.objects[i]
-            elif isinstance(o, Circle) and o.getCenterX() == x and o.getCenterY() == y:
-                del self.objects[i]
+                break
 
     def display(self):
+        self.points_map = {}  # Réinitialise la carte des points à chaque affichage
         s = ""
-        points_map = {}
 
         for obj in self.objects:
             if isinstance(obj, Point):
-                points_map[(obj.getX(), obj.getY())] = obj.getChar()
+                self.points_map[(obj.getX(), obj.getY())] = obj.getChar()
             elif isinstance(obj, Segment):
                 for point in obj.getPoints():
-                    points_map[(point.getX(), point.getY())] = point.getChar()
+                    self.points_map[(point.getX(), point.getY())] = point.getChar()
             elif isinstance(obj, Rectangle):
                 for point in obj.getPoints():
-                    points_map[(point.getX(), point.getY())] = point.getChar()
+                    self.points_map[(point.getX(), point.getY())] = point.getChar()
             elif isinstance(obj, Triangle):
                 for point in obj.getPoints():
-                    points_map[(point.getX(), point.getY())] = point.getChar()
+                    self.points_map[(point.getX(), point.getY())] = point.getChar()
             elif isinstance(obj, Circle):
                 for point in obj.getPoints():
-                    points_map[(point.getX(), point.getY())] = point.getChar()
+                    self.points_map[(point.getX(), point.getY())] = point.getChar()
 
-        for y in range(0, self.engine.getSizeY() + 1):
-            for x in range(0, self.engine.getSizeX() + 1):
-                if (x, y) in points_map:
-                    s += points_map[(x, y)]
+        for y in range(self.engine.getSizeY() + 1):
+            for x in range(self.engine.getSizeX() + 1):
+                if (x, y) in self.points_map and self.points_map[(x, y)] is not None:
+                    s += self.points_map[(x, y)]
                 else:
                     s += self.engine.getChar()
             s += "\n"
