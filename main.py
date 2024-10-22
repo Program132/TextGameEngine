e401f449becd31f695e641f430405c6bdf8e465e
Program <67320703+Program132@users.noreply.github.com>
2024-10-22 21:25:14 +0200

    Test

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+import time
+import keyboard
 from src.Engine import Engine
 from src.Level import Level
-import keyboard
-import time
-from src.Models.Point import Point
+from src.Player import Player
 
 engine = Engine(50, 5, ".")
 mainLevel = Level(engine)
 
-PLAYER_POINT = Point(3, 1)
+PLAYER = Player(3, 1, 'P')
 
-mainLevel.addObject(PLAYER_POINT)
+mainLevel.addObject(PLAYER)
 engine.setCurrentLevel(mainLevel)
 engine.display()
+
+def manageKeysPressed(event):
+    if event.name == "w" or event.name == "z":
+        mainLevel.removeObject(PLAYER.getX(), PLAYER.getY())
+        PLAYER.setY(PLAYER.getY() - 1)
+        mainLevel.addObject(PLAYER)
+        engine.refresh()
+    elif event.name == "s":
+        mainLevel.removeObject(PLAYER.getX(), PLAYER.getY())
+        PLAYER.setY(PLAYER.getY() + 1)
+        mainLevel.addObject(PLAYER)
+        engine.refresh()
+
+
+keyboard.on_press(manageKeysPressed)
 
 running = True
 while running:
