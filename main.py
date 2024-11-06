a5545a9433ff415b446b74ebcbe97472e567f01b
program <clement.gauthier132@orange.fr>
2024-11-06 15:52:28 +0100

    Tests with UIs

--- main.py
+++ main.py
@@ -1,94 +1,17 @@
-import time
-import random
-import keyboard
-
 from src.Engine import Engine
-from src.Level import Level
-from src.Models.Point import Point
-from src.UI.Score import Score, Alignment
-
-SIZE_X = 20
-SIZE_Y = 10
-
-engine = Engine(SIZE_X, SIZE_Y)
-level = Level(engine)
-
-snake = Point(5, 3)
-snake.addTag("snake")
-snake.setChar("S")
-snake.setCanCollide(False)
-
-score = Score()
-score.addScore("Size Snake", 1)
-score.setAlignment(Alignment.CENTER)
+from src.UI import Alignment
+from src.UI.UILevel import UILevel
+from src.UI.UIText import UIText
 
 
-def spawnFruit():
-    x = random.randint(2, SIZE_X)
-    y = random.randint(2, SIZE_Y)
-    p = Point(x, y)
-    p.setChar("*")
-    p.setCanCollide(False)
-    p.addTag("fruit")
-    return p
+engine = Engine()
+menu = UILevel()
+menu.size_x = 100
+text = UIText()
+text.setText("Hello World")
+text.setSizeX(20)
+text.setSizeY(3)
+menu.addObject(text)
 
-
-direction = "down"
-
-
-def keyPressed(event):
-    global direction
-    if event.name == "bas" or event.name == "down":
-        direction = "down"
-    elif event.name == "haut" or event.name == "up":
-        direction = "up"
-    elif event.name == "gauche" or event.name == "left":
-        direction = "left"
-    elif event.name == "droite" or event.name == "right":
-        direction = "right"
-
-
-keyboard.on_press(keyPressed)
-
-level.addObject(snake)
-level.addObject(score)
-fruit = spawnFruit()
-level.addObject(fruit)
-
-engine.setCurrentLevel(level)
+engine.setCurrentLevel(menu)
 engine.display()
-
-running = True
-while running:
-    if (snake.getY() < 0 or snake.getY() > SIZE_Y) or (snake.getX() < 0 or snake.getX() > SIZE_X):
-        print("Game Over")
-        print("Score: " + str(score.getScore("Size Snake")))
-        exit(1)
-
-    level.removeObject(snake.getX(), snake.getY())
-    if direction == "down":
-        snake.setY(snake.getY() + 1)
-    elif direction == "up":
-        snake.setY(snake.getY() - 1)
-    elif direction == "left":
-        snake.setX(snake.getX() - 1)
-    elif direction == "right":
-        snake.setX(snake.getX() + 1)
-    level.addObject(snake)
-
-    # check if fruit at current XY:
-    currentPoint = level.getPoint(snake.getX(), snake.getY())
-    if currentPoint is not None and isinstance(currentPoint, Point) and currentPoint.hasTag("fruit"):
-        score.updateScore("Size Snake", score.getScore("Size Snake") + 1)
-        level.updateLevelScore(score)
-        level.removeObject(fruit.getX(), fruit.getY())
-        fruit = spawnFruit()
-        level.addObject(fruit)
-
-    engine.refresh()
-
-    if keyboard.is_pressed("esc"):
-        running = False
-
-    pause_time = 0.800 / (score.getScore("Size Snake"))
-    time.sleep(pause_time)
