3117ebe2b03a2a22560a9f42d40d0ab2399096bd
program <clement.gauthier132@orange.fr>
2024-11-07 08:10:18 +0100

    Tests sounds

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import time
+
 from src.Engine import Engine
 from src.UI.UILevel import UILevel
 from src.UI.UIButton import UIButton
-
+from src.Sounds.Sounds import Sounds
 
 engine = Engine()
 menu = UILevel()
 menu.size_x = 100
 
+s = Sounds("/home/User/Desktop/note.wav")
+
 def my_button_action():
     print("Button 'clicked' action done")
+    s.play()
 
 button = UIButton(x=5, y=3, sx=15, sy=3, key='a')
 button.setOnClick(my_button_action)
@@ -19,3 +24,5 @@ engine.display()
 
 while True:
     button.listenForClick()
+
+    time.sleep(0.1)
