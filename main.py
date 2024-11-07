fba222761c4293289a437dfe3a0c577a7ea2c658
program <clement.gauthier132@orange.fr>
2024-11-07 07:51:22 +0100

    UIButton test class

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from src.Engine import Engine
-from src.UI import Alignment
 from src.UI.UILevel import UILevel
-from src.UI.UIText import UIText
+from src.UI.UIButton import UIButton
 
 
 engine = Engine()
 menu = UILevel()
 menu.size_x = 100
-text = UIText()
-text.setText("Hello World")
-text.setSizeX(20)
-text.setSizeY(3)
-menu.addObject(text)
 
+def my_button_action():
+    print("Button 'clicked' action done")
+
+button = UIButton(x=5, y=3, sx=15, sy=3, key='a')
+button.setOnClick(my_button_action)
+
+menu.addObject(button)
 engine.setCurrentLevel(menu)
 engine.display()
+
+while True:
+    button.listenForClick()
