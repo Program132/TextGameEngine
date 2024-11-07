b37649703489ebe2e78f6379c54726057ea533cf
program <clement.gauthier132@orange.fr>
2024-11-07 07:53:46 +0100

    Package

--- src/UI/__init__.py
+++ src/UI/__init__.py
@@ -2,5 +2,6 @@ from .Score import Score
 from .Alignment import Alignment
 from .UILevel import UILevel
 from .UIText import UIText
+from .UIButton import UIButton
 
-__all__ = ["Score", "Alignment", "UILevel", "UIText"]
+__all__ = ["Score", "Alignment", "UILevel", "UIText", "UIButton"]
