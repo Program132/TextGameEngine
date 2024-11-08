026cc41e90be668d4709d79c48136fad5a6d158e
Program <67320703+Program132@users.noreply.github.com>
2024-11-08 19:27:01 +0100

    Package

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from .Engine import Engine
 from .Level import Level
 from .Player import Player
+from .Inputs import Inputs
 
-__all__ = ["Engine", "Level", "Player"]
+__all__ = ["Engine", "Level", "Player", "Inputs"]
